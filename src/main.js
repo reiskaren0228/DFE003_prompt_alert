2af4e513d8bd0dd93036a14b9d52cb74821aa659
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-05-31 18:42:20 -0300

    atualização desafio prompt

--- src/main.js
+++ src/main.js
@@ -1,49 +1,60 @@
-/*- Elabore um programa que leia um numero e devolva para o usuário o dobro do número informado.
+function resultado(){
+
+const numero = document.getElementById("input__num__digitado").value;
+
+if(numero%2==0) {
+    const texto =`O número ${numero} é par. O ${numero} x 2 é: ${numero*2}`;
+    document.getElementById("resultado_par_impar").innerHTML = texto;
+       
+}else {
+    const texto =`O número ${numero} é impar. O ${numero} x 3 é: ${numero*3}`;
+    document.getElementById("resultado_par_impar").innerHTML = texto;
+    
+}
+
+const formulario = document.getElementById("section__formulario__par_impar");
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault();
+        })
+
+}
+
+function valor__evento(){
+const valorJantar = document.getElementById("input__valor_jantar");
+const taxaGarcon = document.getElementById("input__num__taxa");
+
+const valorTotal = parseInt(valorJantar.value) + (parseInt((parseInt(taxaGarcon.value) / 100)* parseInt(valorJantar.value)));
+
+const texto = `O valor do jantar é R$ ${valorJantar.value} reais, e o valor total da conta a ser pago com os 10% da taxa de atendimento do garçom é R$ ${valorTotal} reais.`; 
+
+document.getElementById("resultado__jantar").innerHTML= texto;
+
+const formulario = document.getElementById("section__formulario__jantar");
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault();
+        })
+
+}
 
 
-~~~javascript
+function valor__pizzaria() {
 
-  Entrada de dados: ler um número.
-  Processamento: calcular o dobro.
-  Saída: informar o resultado.
+    const valorConta = document.getElementById("input__valor_consumo");
+    const qtdPessoa = document.getElementById("qtd__num__pessoas");
+    const valorTotalPessoa = parseInt(valorConta.value)/parseInt(qtdPessoa.value);
+    
+    const texto = `O valor a ser pago por pessoa será de R$ ${valorTotalPessoa} reais.`;
+  
+    document.getElementById("valorTotal").innerHTML= texto;
+ 
+    const formulario = document.getElementById("section__formulario__evento");
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault();
+        })
 
-~~~*/
-let num = prompt("Digite um número: ");
-
-const numero = num * 2;
-
-alert("O dobro do número " + num + " é " + numero)
-
-/*- Elabore um programa que calcule o valor do jantar, sendo o valor total do jantar composto pela conta junto com a taxa de atendimento do garçom. 
-
-~~~javascript
-
-  Entrada de dados: ler o valor da conta.
-  Processamento: calcular junto com o valor do custo do garçom.
-  Saída: informar o resultado.
-
-~~~*/
-
-let valorJantar = parseFloat(prompt("Informe o valor do jantar: R$ "));
-let taxaGarcon = parseFloat(prompt("Digite qual a taxa de serviço do garçon em %: "));
-let valorTotal = valorJantar + ((taxaGarcon / 100)*valorJantar);
+}
 
 
-alert("O valor do jantar é R$ " + valorJantar + " reais, e o valor total da conta a ser pago com os 10% da taxa de atendimento do garçom é R$ " + valorTotal + " reais.")
 
+   
 
-/*- Elabore um programa para uma pizzaria, o qual leia o valor total de uma conta e quantos clientes vão pagá-la. Calcule e informe o valor a ser pago por cliente. 
-
-~~~javascript
-
-  Entrada de dados: ler o valor da conta e a quantidade de clientes.
-  Processamento: calcular o valor a ser pago por cliente.
-  Saída: informar o resultado.
-
-~~~*/
-
-let valorConta = parseFloat(prompt("Informe o valor total da conta do consumo na pizzaria: R$ "));
-let qtdPessoa = parseInt(prompt("Informe a quantidade de pessoa que irão dividir a conta: "));
-const valorTotalPessoa = valorConta / qtdPessoa;
-
-alert(" O valor a ser pago por pessoa será de R$ " + valorTotalPessoa + " reais.")
